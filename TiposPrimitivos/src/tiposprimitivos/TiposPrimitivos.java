9340ac81a9383094efa23f452a28f13d383e1569
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-09-23 21:20:40 +0200

    Create TiposPrimitivos.java

---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author Maria333
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte _byte = 1;
+        System.out.println("Vamos a ver el rango del tipo BYTE");
+        System.out.println("El rango máximo es: " + Byte.MAX_VALUE);
+        System.out.println("El rango mínimo es: " + Byte.MIN_VALUE);
+    }
+    
+}
